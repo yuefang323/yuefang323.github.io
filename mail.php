3249ecf0e9e0dc31e93270a0cb98b10e5b93e91e
Fang Yue <yuefang323@gmail.com>
2022-06-30 08:48:57 -0700

    add contact

--- mail.php
+++ mail.php
@@ -3,7 +3,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "info@yourmail.com";
+        $mail_to = "fangyue202201@gmail.com";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
